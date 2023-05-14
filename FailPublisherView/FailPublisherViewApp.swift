b51c7873e0d3ba3b614ff6d939270d419c09d6d3
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-14 21:26:30 +0500

    Initial Commit

---
+++ FailPublisherView/FailPublisherViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FailPublisherViewApp.swift
+//  FailPublisherView
+//
+//  Created by sss on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FailPublisherViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
